f1a5908c282f0aebf49b04130ee1ec8d6ee7c5ec
aakrut <aakrutdabhi@gmail.com>
2022-06-08 15:48:04 +0530

    user-model&folder-structure

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+const { register, login } = require("../controllers/auth/auth");
 
 const router = express.Router();
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
+});
+
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  email: Joi.string().email().required(),
+});
+
+router.route("/register").post(validator.body(registerSchema), register);
+router.route("/login").post(validator.body(loginSchema), login);
 
 module.exports = router;
