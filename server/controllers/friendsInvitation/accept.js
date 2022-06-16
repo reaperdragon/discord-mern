ccf5ee39ca5104e1f49df43656183b48aa8ecdff
aakrut <aakrutdabhi@gmail.com>
2022-06-16 20:45:51 +0530

    accept-controller

--- server/controllers/friendsInvitation/accept.js
+++ server/controllers/friendsInvitation/accept.js
@@ -1,5 +1,27 @@
-const accept = async (req, res) => {
-   return res.send('Accept')
-}
+const FriendsInvitation = require("../../models/friendInvitation");
+const { StatusCodes } = require("http-status-codes");
+const friendsUpdates = require("../../socketHandlers/updates/friends");
 
-module.exports = {accept}
+const accept = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { userId } = req.user;
+
+    const invitationExists = await FriendsInvitation.exists({ _id: id });
+
+    if (invitationExists) {
+      await Invitation.findByIdAndDelete(id);
+    }
+
+    friendsUpdates.updateFriendsPendingInvitations(userId);
+
+    return res.status(StatusCodes);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send("Something went Wrong Please Try Again.");
+  }
+};
+
+module.exports = { accept };
